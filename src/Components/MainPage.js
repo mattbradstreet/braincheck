510350ce439458ffc3074d87ffb492a9a6346c78
matt <w1326166@my.westminster.ac.uk>
2024-03-31 19:54:15 +0100

    Text added to Data Vis

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -21,6 +21,9 @@ var bt_graph_value4 = 0.0;
 var az_graph_value = 0.0;
 var pk_graph_value = 0.0;
 
+var az_graph_colour = 'powderBlue';
+var pk_graph_colour = 'powderBlue';
+
 //load models
 async function loadModel() {
     bt_model = await tf.loadLayersModel('/tfjs_btmodel/model.json');
@@ -111,14 +114,24 @@ function MainPage() {
         if (az_max_value > 0.5)
         {
             az_position = 1;
+            az_graph_colour = 'firebrick';
         }
+        if (az_max_value < 0.0001)  
+        {
+            az_max_value = 0.00;
+        }  
 
         var pk_max_value = pk_prediction[0];
         var pk_position = 0;
         if (pk_max_value > 0.5)
         {
             pk_position = 1;
-        }      
+            pk_graph_colour = 'firebrick';
+        }
+        if (pk_max_value < 0.0001)  
+        {
+            pk_max_value = 0.00;
+        }    
 
         setModelResult("Brain Tumour: " + bt_class_names[bt_position] + " - " + bt_max_value + "\n" 
                         + "Alzheimers: " + az_class_names[az_position] + " - " + az_max_value + "\n"
@@ -152,6 +165,14 @@ function MainPage() {
         setUploadedImage();
         setAppMessage('');
         setModelResult('');
+        bt_graph_value1 = 0.0;
+        bt_graph_value2 = 0.0;
+        bt_graph_value3 = 0.0;
+        bt_graph_value4 = 0.0;
+        az_graph_value = 0.0;
+        pk_graph_value = 0.0;
+        az_graph_colour = 'powderBlue';
+        pk_graph_colour = 'powderBlue';
     }      
 
 
@@ -170,25 +191,32 @@ function MainPage() {
                     </div>
                 </div>
 
-                <div className="column">                 
+                <div className="column">            
                     <div style={{width:'100%', height:'60%', borderRadius:'5px', border:'1px solid #000', backgroundColor:'beige'}}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="100%"
-                            height="100%"
-                            viewBox="0 0 100 100"
-                            >
-                            <rect x="-15" y="8" width="130" height="42" stroke="black" fill="transparent" stroke-width="0.3"/>
-                            <rect x="-15" y="55" width="130" height="15" stroke="black" fill="transparent" stroke-width="0.3"/>
-                            <rect x="-15" y="75" width="130" height="15" stroke="black" fill="transparent" stroke-width="0.3"/>
-                            
-                            <rect x="-15" y="10" width={bt_graph_value1} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
-                            <rect x="-15" y="20" width={bt_graph_value2} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
-                            <rect x="-15" y="30" width={bt_graph_value3} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
-                            <rect x="-15" y="40" width={bt_graph_value4} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
 
-                            <rect x="-15" y="55" width={az_graph_value} height="15" stroke="black" fill="powderBlue" stroke-width="0.3"/>
-                            <rect x="-15" y="75" width={pk_graph_value} height="15" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                            <text x="-15" y="8" style={{fontSize:'25%'}}>Brain Tumour</text>
+                            <rect x="-15" y="10" width="130" height="42" stroke="black" fill="transparent" stroke-width="0.1"/>
+                            <text x="-23" y="17" style={{fontSize:'25%'}}>Gli</text>
+                            <rect x="-15" y="12" width={bt_graph_value1} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                            <text x="-23" y="27" style={{fontSize:'25%'}}>Me</text>
+                            <rect x="-15" y="22" width={bt_graph_value2} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                            <text x="-23" y="37" style={{fontSize:'25%'}}>Pit</text>
+                            <rect x="-15" y="32" width={bt_graph_value4} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                            <text x="-23" y="47" style={{fontSize:'25%'}}>No</text>
+                            <rect x="-15" y="42" width={bt_graph_value3} height="8" stroke="black" fill="powderBlue" stroke-width="0.3"/>
+                            
+                            <text x="-15" y="59" style={{fontSize:'25%'}}>Alzheimer's</text>
+                            <text x="-25" y="68" style={{fontSize:'25%'}}>Neg</text>
+                            <text x="117" y="68" style={{fontSize:'25%'}}>Pos</text>
+                            <rect x="-15" y="61" width="130" height="12" stroke="black" fill="transparent" stroke-width="0.1"/>
+                            <rect x="-15" y="61" width={az_graph_value} height="12" stroke="black" fill={az_graph_colour} stroke-width="0.3"/>
+
+                            <text x="-15" y="80" style={{fontSize:'25%'}}>Parkinson's</text>
+                            <text x="-25" y="89" style={{fontSize:'25%'}}>Neg</text>
+                            <text x="117" y="89" style={{fontSize:'25%'}}>Pos</text>
+                            <rect x="-15" y="82" width="130" height="12" stroke="black" fill="transparent" stroke-width="0.1"/>
+                            <rect x="-15" y="82" width={pk_graph_value} height="12" stroke="black" fill={pk_graph_colour} stroke-width="0.3"/>
                         </svg>
                     </div>
                     <p>Data Visualisation</p>   
